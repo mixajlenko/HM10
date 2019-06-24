212920f83c35b9f49e3935809d699b16e5212028
Alexander Mykhailenko <sexperiax8986@gmail.com>
2019-06-24 21:09:42 +0300

    IO streams.

---
+++ src/ListOfNubersDemo.java
@@ -0,0 +1,7 @@
+
+public class ListOfNubersDemo {
+    public static void main(String[] args) throws ScaryException {
+        Application app = new Application();
+        app.logicOfApplication();
+    }
+}
